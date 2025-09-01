8ba7d27bf7410e2e2389ba588e6f9cc1ffbf42e5
oabidi <oabidi@hs-koblenz.de>
2025-09-01 17:59:35 +0200

    refactor: Update string formatting and implement plant detail view
    This commit introduces two main changes: it refactors the string formatting in `MainActivity` and implements the functionality for the `PlantDetailActivity` to display plant details.
    
    - In `MainActivity.java`, the `String.format` calls have been simplified by using a static import for `java.lang.String.format`.
    - `PlantDetailActivity.java` now retrieves the plant's name and description from the `Intent` extras.
    - The retrieved data is then displayed in the corresponding `TextViews` (`detail_name` and `detail_description`).
    - An ID (`@+id/main`) has been added to the root `ScrollView` in `activity_plant_detail.xml` to support window insets handling.

--- app/src/main/java/de/oabidi/pflanzenbestandundlichttest/PlantDetailActivity.java
+++ app/src/main/java/de/oabidi/pflanzenbestandundlichttest/PlantDetailActivity.java
@@ -1,6 +1,7 @@
 package de.oabidi.pflanzenbestandundlichttest;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,15 @@ public class PlantDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_plant_detail);
+
+        String name = getIntent().getStringExtra("name");
+        String description = getIntent().getStringExtra("description");
+
+        TextView nameView = findViewById(R.id.detail_name);
+        TextView descriptionView = findViewById(R.id.detail_description);
+        nameView.setText(name);
+        descriptionView.setText(description);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
