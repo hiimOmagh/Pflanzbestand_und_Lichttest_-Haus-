1f886e1847b02472207df3c547e7a65c51b30bac
oabidi <oabidi@hs-koblenz.de>
2025-09-11 17:21:21 +0200

    refactor: Remove debug crash button
    This commit removes the "Test Crash" button previously available in debug builds.
    
    - In `MainActivity.java`:
        - Removed the code that programmatically created and added the "Test Crash" button to the view in debug builds.
    - In `res/values/strings.xml` (and its Spanish, German, and French translations):
        - Removed the `test_crash` string resource.

--- app/src/main/java/de/oabidi/pflanzenbestandundlichttest/MainActivity.java
+++ app/src/main/java/de/oabidi/pflanzenbestandundlichttest/MainActivity.java
@@ -3,12 +3,8 @@ package de.oabidi.pflanzenbestandundlichttest;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -54,26 +50,6 @@ public class MainActivity extends AppCompatActivity implements MainView {
             presenter.onNavigationItemSelected(item.getItemId()));
 
         presenter.onCreate(savedInstanceState, getIntent());
-
-        if (BuildConfig.DEBUG) {
-            // Creates a button that mimics a crash when pressed
-            Button crashButton = new Button(this);
-            crashButton.setText(R.string.test_crash);
-            crashButton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    throw new RuntimeException(getString(R.string.test_crash)); // Force a crash
-                }
-            });
-
-            // Get the root FrameLayout
-            FrameLayout rootView = (FrameLayout) findViewById(android.R.id.content);
-            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.WRAP_CONTENT,
-                FrameLayout.LayoutParams.WRAP_CONTENT);
-            params.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
-            params.bottomMargin = 180; // Increased from 100 to 180
-            rootView.addView(crashButton, params);
-        }
 
     }
 
